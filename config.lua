3a1f3d17a46e9d6a9b1680b9dd6d244aa6612b32
benny1x <peston66@gmail.com>
2026-07-15 00:19:51 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,26 +1,15 @@
 Config = {}
 
-Config.Debug = true
+Config.Debug = false
 
 Config.Framework = 'auto'
-
--- Inventory provider:
--- auto | ox | qs | codem | tgiann | core | ps | lj | qb | none
 Config.Inventory = 'auto'
-
--- Target provider:
--- auto | ox | qb | qtarget | none
 Config.Target = 'auto'
-
--- Dispatch provider:
--- auto | none | cd_dispatch | ps-dispatch | core_dispatch | qs-dispatch
--- rcore_dispatch | codem-dispatch | origen_police | lb-tablet | tk_dispatch
 Config.Dispatch = 'auto'
 
 Config.DispatchJobs = {
     'police',
 }
-
 
 Config.Medical = {
     Provider = 'auto',
